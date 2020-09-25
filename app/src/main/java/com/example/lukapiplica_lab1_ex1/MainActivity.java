ce8ecbb2d1c13b5768edf45798203593f0132cee
n01294879 <n01294879@users.noreply.github.com>
2020-09-25 15:23:12 -0400

    Final Commit

--- app/src/main/java/com/example/lukapiplica_lab1_ex1/MainActivity.java
+++ app/src/main/java/com/example/lukapiplica_lab1_ex1/MainActivity.java
@@ -4,9 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onStart()
-    {
-        super.onStart();
     }
 }
